ecce11d0d20212dbeb6655815e202956a215cf79
smoes <smoes@localhost.localdomain>
2014-01-19 22:36:18 +0100

    Fixing quotation marks for auto embedding in quotes and spoilers

--- addons/plugins/Vimeo/plugin.php
+++ addons/plugins/Vimeo/plugin.php
@@ -3,7 +3,7 @@ if (!defined("IN_ESOTALK")) exit;
  
  ET::$pluginInfo["Vimeo"] = array(
 	"name" => "Vimeo",
-	"description" => "Autoembedding vimeo links"
+	"description" => "Autoembedding vimeo links",
 	"version" => "1.0",
 	"author" => "smoes",
 	"authorEmail" => "smoesorino@gmail.com",
@@ -11,7 +11,7 @@ if (!defined("IN_ESOTALK")) exit;
 	"license" => "GPLv2"
 );
 
-class ETPlugin_Soundcloud extends ETPlugin {
+class ETPlugin_Vimeo extends ETPlugin {
 
 	public function handler_conversationController_renderBefore($sender){
 			$sender->addCSSFile($this->getResource("vimeo.css"));
@@ -29,14 +29,14 @@ class ETPlugin_Soundcloud extends ETPlugin {
 	
         public function handler_format_beforeFormat( $sender ){
 
-                $regexp = '/vimeo\.com\/([0-9]+)/';
+                $regexp = '/https?:\/\/[www\.]?vimeo\.com\/([0-9]+)/';
                 
                 // replace with emebdding like shown here
                 // http://blog.soundcloud.com/2009/07/28/soundcloud-player-in-forums-5-step-guide-for-soundcloud-bb-code/
 
   		while (preg_match($regexp, $sender->content)) {
                         $sender->content = preg_replace($regexp,
-                                '<div style="width: 600px; margin: 0 auto;"><iframe src="http://player.vimeo.com/video/' . $1 . '" width="600" height="400" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>', $sender->content);
+                                "<div style='width: 600px; margin: 0 auto;'><iframe src='http://player.vimeo.com/video/$1' width='600' height='400' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>", $sender->content);
 		}
 
 
