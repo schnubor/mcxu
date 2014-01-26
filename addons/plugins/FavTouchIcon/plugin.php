b25b2dd2dc0e3da6c133676587b7021b490c343b
schnubor <info@chko.org>
2014-01-26 20:26:24 +0100

    added iOS homescreen icon support

--- addons/plugins/FavTouchIcon/plugin.php
+++ addons/plugins/FavTouchIcon/plugin.php
@@ -15,6 +15,10 @@ class ETPlugin_FavTouchIcon extends ETPlugin {
 
 	public function handler_init( $sender ){
 		$sender->addToHead("<link rel='shortcut icon' href='".getWebPath($this->getResource("favicon.ico"))."'>");
+		$sender->addToHead("<link href='".getWebPath($this->getResource("apple-touch-icon.png"))."' rel='apple-touch-icon' />");
+		$sender->addToHead("<link href='".getWebPath($this->getResource("apple-touch-icon-76x76.png"))."' rel='apple-touch-icon' sizes='76x76' />");
+		$sender->addToHead("<link href='".getWebPath($this->getResource("apple-touch-icon-120x120.png"))."' rel='apple-touch-icon' sizes='120x120' />");
+		$sender->addToHead("<link href='".getWebPath($this->getResource("apple-touch-icon-152x152.png"))."' rel='apple-touch-icon' sizes='152x152' />");
 	}
 }
 ?>
